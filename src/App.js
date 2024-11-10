923ae0f731593e8340ccdaa470c0e16f9d824e72
xshandy <shandygshek@hotmail.co.uk>
2024-11-10 12:52:23 +0000

    added footer with a link to my git

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Shandy Shek and is{" "}
+        <a
+          href="https://github.com/xshandy/react-weatherapp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
